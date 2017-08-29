c75e8fd1ff086e973416567599a67874b747a7b9
FalseProposition <1344602119@qq.com>
2017-08-29 20:28:55 +0800

    Create Compare.java

---
+++ Simple Calculator/Android App/Compare.java
@@ -0,0 +1,47 @@
+/**
+ * 用于进行运算符和数值的比较运算
+ */
+
+public class Compare {
+    static char Precede(char op1, char op2)//比较运算符的优先级
+    {
+        switch (op1) {
+            case '+':
+            case '-':
+                if (op2 == '+' || op2 == '-' || op2 == ')' || op2 == '#') return '>';
+                else if (op2 == '*' || op2 == '/' || op2 == '(') return '<';
+            case '*':
+            case '/':
+                if (op2 == '+' || op2 == '-' || op2 == '*' || op2 == '/' || op2 == ')' || op2 == '#')
+                    return '>';
+                else if (op2 == '(') return '<';
+            case '(':
+                if (op2 == '+' || op2 == '-' || op2 == '*' || op2 == '/' || op2 == '(' || op2 == '^')
+                    return '<';
+                else if (op2 == ')') return '=';
+            case ')':
+                if (op2 == '+' || op2 == '-' || op2 == '*' || op2 == '/' || op2 == ')' || op2 == '#')
+                    return '>';
+            case '#':
+                if (op2 == '+' || op2 == '-' || op2 == '*' || op2 == '/' || op2 == '(') return '<';
+                else if (op2 == '#') return '=';
+        }
+        return 0;
+    }
+
+    static int operate(int a, char c, int b)//对数值进行四则运算
+    {
+        switch (c) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                return a / b;
+        }
+        return 0;
+    }
+
+}
